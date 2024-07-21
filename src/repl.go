c07b49b24a9fc5edbfa348ac4c45aaf05f71f685
fadhil riyanto <me@fadev.org>
2024-07-21 19:05:58 +0700

    add support for commands
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- src/repl.go
+++ src/repl.go
@@ -3,12 +3,64 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
+func handleCommand(data string) string {
+	if data[0] == '/' {
+		fn := strings.Split(data[1:], " ")
+
+		if fn[0] == "genbuf" {
+			ourint, err := strconv.Atoi(fn[2])
+			if err != nil {
+				panic(err)
+			}
+			return Genbuf(fn[1], ourint)
+		}
+
+		if fn[0] == "genrandbuf" {
+			ourint, err := strconv.Atoi(fn[1])
+			if err != nil {
+				panic(err)
+			}
+			return Genrandbuf(ourint)
+		}
+
+		// reserved
+		if fn[0] == "n" {
+			return "\n"
+		}
+		if fn[0] == "nr" {
+			return "\n\r"
+		}
+		if fn[0] == "nr2" {
+			return "\n\r\r\n"
+		}
+	}
+	return data
+}
+
+func readFromserver(conn net.Conn) {
+	for {
+		buf := make([]byte, 1024)
+
+		_, err := conn.Read(buf)
+		if err != nil {
+			log.Println("Error reading data from connection:", err)
+			return
+		}
+		recvtext := string(buf)
+		fmt.Print("[reply] ", recvtext)
+	}
+}
+
 func ReplEventloop(conn net.Conn) {
+	go readFromserver(conn)
+
 	for {
 		consoleReader := bufio.NewReaderSize(os.Stdin, 1)
 		inputtext, _ := consoleReader.ReadString('\n')
@@ -19,8 +71,9 @@ func ReplEventloop(conn net.Conn) {
 			os.Exit(0)
 		}
 
-		// fmt.Println("$ ", inputtext)
-		conn.Write([]byte(strings.TrimSuffix(inputtext, "\n")))
+		commandresult := handleCommand(strings.TrimSuffix(inputtext, "\n"))
+
+		conn.Write([]byte(commandresult))
 
 	}
 
